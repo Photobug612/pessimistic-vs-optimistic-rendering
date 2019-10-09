0509d8820a2d8e6c736898342916c0882430330b
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-10-09 11:31:33 -0600

    changed backend

--- animals/db/schema.rb
+++ animals/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_013326) do
+ActiveRecord::Schema.define(version: 2019_10_09_163145) do
 
-  create_table "images", force: :cascade do |t|
-    t.string "url"
+  create_table "animals", force: :cascade do |t|
+    t.string "image_url"
     t.integer "likes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- frontend/app.js
+++ frontend/app.js
@@ -1,42 +1,42 @@
-fetch('http://localhost:3000/images')
+fetch('http://localhost:3000/animals')
     .then(response => response.json())
-    .then(imageData => createImages(imageData))
+    .then(animalData => createAnimals(animalData))
 
-const createImages = (imageData) => {
-    imageData.forEach(addEachImage)
-
+const createAnimals = (animalData) => {
+    animalData.forEach(addEachAnimal)
 }
 
-const addEachImage = image => {
+const addEachAnimal = animal => {
     const $ul = document.querySelector('ul')
     const $li = document.createElement('li')
 
     const picture = document.createElement('img')
-    picture.src = image.url
+    console.log("animal image", animal.image_url)
+    picture.src = animal.image_url
 
     const likes = document.createElement('p')
-    likes.innerText = `Likes: ${image.likes}`
+    likes.innerText = animal.likes
 
     const likesButton = document.createElement('button')
     likesButton.innerText = 'Add Like'
-    likesButton.addEventListener('click', (event) => addClick(event, image, image.id))
+    likesButton.addEventListener('click', (event) => addClick(event, animal, animal.id))
 
     const removeAnimal = document.createElement('button')
     removeAnimal.innerText = 'Delete'
-    removeAnimal.addEventListener('click', () => remove(event, image.id))
+    removeAnimal.addEventListener('click', () => remove(event, animal.id))
 
     $li.append(picture, likes, likesButton, removeAnimal)
     $ul.appendChild($li)
 }
 
-const imageForm = document.querySelector('#image_form')
-imageForm.addEventListener("submit", event => {
+const animalForm = document.querySelector('#animal_form')
+animalForm.addEventListener("submit", event => {
     event.preventDefault()
 
-    const formData = new FormData(image_form)
+    const formData = new FormData(animal_form)
 
     let animal = {
-        url: formData.get("url"),
+        image_url: formData.get("image_url"),
         likes: 0
     }
 
@@ -47,47 +47,43 @@ imageForm.addEventListener("submit", event => {
 //POST - pessimistic rendering
 const postAnimal = (animal) => {
     console.log("animal in post", animal)
-    fetch("http://localhost:3000/images", {
+    fetch("http://localhost:3000/animals", {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-type': 'application/json'
         },
         body: JSON.stringify({
-            url: animal.url,
+            image_url: animal.image_url,
             likes: animal.likes
         })
 
     }).then(response => response.json())
-        .then(res => addEachImage(res))
+        .then(res => addEachAnimal(res))
 }
 
 //PATCH - optimistic rendering
-const addClick = (event, image, id) => {
+const addClick = (event, animal, id) => {
     const target = event.target.parentNode.firstChild.nextSibling
-    const likes = image.likes++
-    target.innerText = `Likes: ${likes + 1}`
+    const animalLikes = parseInt(target.innerText)
+    const likes = animal.likes++
+    target.innerText = animalLikes + 1
 
-
-
-    fetch(`http://localhost:3000/images/${id}`, {
+    fetch(`http://localhost:3000/animals/${id}`, {
         headers: { "Content-Type": "application/json; charset=utf-8" },
         method: 'PATCH',
         body: JSON.stringify({
-            url: image.url,
+            image_url: animal.image_url,
             likes: likes,
         })
     })
+
 }
 
-//POST - pessimistic rendering
-// var formData = new FormData(document.getElementById('test-form'));
-
-
-//delete
+//delete -- optimisitic
 const remove = (event, id) => {
     event.target.parentNode.remove()
-    fetch(`http://localhost:3000/images/${id}`, {
+    fetch(`http://localhost:3000/animals/${id}`, {
         headers: { "Content-Type": "application/json" },
         method: 'DELETE',
         body: JSON.stringify({ id })
