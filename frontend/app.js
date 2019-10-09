55f98f2d957a16b74ac6ad2b3854ca0266289984
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-10-09 11:50:20 -0600

    added boilerplate code

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,14 @@ fetch('http://localhost:3000/animals')
     .then(response => response.json())
     .then(animalData => createAnimals(animalData))
 
+const createAnimals = (animalData) => {
+    animalData.forEach(addEachAnimal)
+}
+
+const addEachAnimal = animal => {
+    
+}
+
 //POST - pessimistic rendering
 
 //PATCH likes - optimistic rendering
